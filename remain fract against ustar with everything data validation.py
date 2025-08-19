0b452c199086ba173bfb9c886e55d13073163996
ytm13 <yinthumin111@gmail.com>
2025-08-19 12:02:45 +0800

    minor changes

--- remain fract against ustar with everything data validation.py
+++ remain fract against ustar with everything data validation.py
@@ -142,30 +142,3 @@ for R in R_list:
 
         plt.tight_layout()
         plt.show()
-
-'''
-results1[mean_f_prime_biasi] = remain_frac_values_biasi
-results2[mean_f_prime_nonbiasi] = remain_frac_values_nonbiasi
-
-plt.figure(figsize=(8,5))
-
-# RUN plots
-plt.scatter(data["X9"], data["Y9"], marker="o", label="RUN9")
-plt.scatter(data["X10"], data["Y10"], marker="s", label="RUN10")
-plt.scatter(data["X15"], data["Y15"], marker="^", label="RUN15")
-
-# biasi plot
-plt.plot(fric_velo_range, remain_frac_values_biasi, 'o-', markersize=4, label='Biasi RNR Model')
-# non biasi plot
-plt.plot(fric_velo_range, remain_frac_values_nonbiasi, 'o-', markersize=4, label='Base RNR Model')
-
-plt.title("10μm alumina particles")
-plt.xlabel("friction velocity (m/s)")
-plt.ylabel("remainded fraction")
-plt.xscale("log")
-plt.legend()
-plt.grid(True)
-
-plt.tight_layout()
-plt.show()
-'''
